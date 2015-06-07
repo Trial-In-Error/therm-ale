4c55271efb34ca19c8944ede2fc355d4c3df2d31
Trial-In-Error <ashton.eby@gmail.com>
2015-06-07 18:16:34 -0400

    prompt/file io groundwork laid.

--- server.js
+++ server.js
@@ -1,45 +1,83 @@
+var settings = require('./settings.json');
+var prompt = require('prompt');
+
+if(!process.argv[2]) {
+	console.log('No file name specified for logging. Logging will be disabled.');
+	settings.logging = false;
+}
+
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
+
+var serial = new SerialPort("COM3", {
+	parser: serialport.parsers.readline("\n")
+}, false);
+
+prompt.start();
 var nodemailer = require('nodemailer');
-var settings = require("./settings.json");
-
-var sp = new SerialPort("COM3", {
-  parser: serialport.parsers.readline("\n")
-});
-
-sp.on('open', function(){
-  console.log('Serial Port Open:');
- 	// send mail with defined transport object
-	transporter.sendMail(mailOptions, function(error, info){
-	    if(error){
-	        console.log(error);
-	    }else{
-	        console.log('Message sent: ' + info.response);
-	    }
+var fs = require('fs');
+if(settings.logging) {
+	fs.open('./'+process.argv[2], 'wx', function(err, fd) {
+		if(err && err.code === 'EEXIST') {
+			console.log('The file '+process.argv[2]+' already exists. Append to file?');
+			prompt.get(['shouldAppend'], function(err, result) {
+				if (err) { return onError(err); }
+				if (result.shouldAppend.toLowerCase() === 'y' || result.shouldAppend.toLowerCase() === 'yes') {
+					console.log('Will attempt to append.');
+					serial.open(function (error) {
+						if ( error ) {
+							console.log('failed to open: '+error);
+							process.exit(1);
+						} else {
+							console.log('open');
+							// send mail with defined transport object
+							// transporter.sendMail(mailOptions, function(error, info){
+							// 	if(error){
+							// 		console.log(error);
+							// 	}else{
+							// 		console.log('Message sent: ' + info.response);
+							// 	}
+							// });
+							serial.on('data', serialDataHandler);
+						}
+					});
+				} else if (result.shouldAppend.toLowerCase() === 'n' || result.shouldAppend.toLowerCase() === 'no') {
+					console.log('Will crash.');
+					process.exit(0);
+				} else {
+					console.log('Invalid input; will retry.');
+				}
+			});
+		} else if(err) {
+			console.log('This error is NOT ok.');
+			process.exit(1);
+		}
 	});
-  sp.on('data', function(data){
-      console.log(data);
-  });
-});
+}
+
+function serialDataHandler(data) {
+		console.log(data);
+}
 
 // create reusable transporter object using SMTP transport
 var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: settings.user,
-        pass: settings.pass
-    }
+	service: 'Gmail',
+	auth: {
+		user: settings.user,
+		pass: settings.pass
+	}
 });
-
-// NB! No need to recreate the transporter object. You can use
-// the same transporter object for all e-mails
 
 // setup e-mail data with unicode symbols
 var mailOptions = {
-    from: 'Ashton Eby ✔ <tylhandrias@gmail.com>', // sender address
-    to: 'tylhandrias@gmail.com', // list of receivers
-    subject: 'Hello from the Microview ✔', // Subject line
-    text: 'Hello world ✔, this is plaintext body', // plaintext body
-    html: '<b>Hello world, this is HTML body ✔</b>' // html body
+	from: 'Ashton Eby ✔ <tylhandrias@gmail.com>', // sender address
+	to: 'tylhandrias@gmail.com', // list of receivers
+	subject: 'Hello from the Microview ✔', // Subject line
+	text: 'Hello world ✔, this is plaintext body', // plaintext body
+	html: '<b>Hello world, this is HTML body ✔</b>' // html body
 };
 
+function onError(err) {
+	console.log(err);
+	return 1;
+}
